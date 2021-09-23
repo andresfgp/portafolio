1ea36edac08a986387f9d06251cd2eb7f3e437d8
andresfgp <64049320+andresfgp@users.noreply.github.com>
2021-09-22 21:31:33 -0500

    modal

--- src/Modal/components/CertificateModal.js
+++ src/Modal/components/CertificateModal.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import '../assets/styles/components/CertificateModal.css'
+import React from 'react';
+import '../assets/styles/components/CertificateModal.scss';
 
 const CertificateModal = (props) => {
-    const { setOpenModal, urlCertificate } = props;
-    const handleOnClick=()=>{ 
-        setOpenModal(false)
-    }
-    return (
-        <div className="CertificateModal">
-            <img src={urlCertificate} alt="" />
-            <button 
-            className='button'
-            onClick={handleOnClick} //funcion
-          >+</button>  
-        </div>
-    )
-}
+  const { setOpenModal, urlCertificate } = props;
+  const handleOnClick = () => {
+    setOpenModal(false);
+  };
+  return (
+    <div className='CertificateModal'>
+      <img src={urlCertificate} alt='' />
+      {/* eslint-disable-next-line react/button-has-type */}
+      <button
+        className='button'
+        onClick={handleOnClick}
+      >
+        +
+      </button>
+    </div>
+  );
+};
 
-export default CertificateModal
+export default CertificateModal;
